a3bdbf7e53aea707f0d72aaba968a2f82fbad63a
randze <65006180+randze@users.noreply.github.com>
2020-07-26 01:50:40 -0400

    Added page routers

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ app.use(express.urlencoded({ extended: true }))
 // serve public folder
 app.use(express.static('public'))
 
+// page routers
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('/notes', function(req, res) {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
 
 // app listener
 app.listen( PORT, function(){
